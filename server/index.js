ed7d193ebd789a98044e504ce0cb02c91e9c7b48
01XASSASSIN <hariharanuv7@gmail.com>
2024-04-12 22:21:55 +0530

    user register route

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { error } from "console";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
 /*CONFIGURATIONS*/
 
@@ -35,6 +36,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/*Route with files*/
+app.post("/auth/register", upload.single("picture"), register);
 
 /*mongoose setup*/
 
