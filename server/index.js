b811fd90b71cfef3493a052517595dd2a6c51ffd
01XASSASSIN <hariharanuv7@gmail.com>
2024-04-14 10:27:35 +0530

    User Routes Setup

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+
 import { register } from "./controllers/auth.js";
 
 /*CONFIGURATIONS*/
@@ -42,6 +45,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 //Routes
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /*mongoose setup*/
 
