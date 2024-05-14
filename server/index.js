e9a6ed58e9f57cd9f00243e1a9ea15f312215eea
01XASSASSIN <hariharanuv7@gmail.com>
2024-05-14 21:31:35 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -32,7 +32,13 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://sociopedia-frontend-eta.vercel.app",
+    methods: "*",
+    credentials: true,
+  })
+);
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /*FILE STORAGE*/
