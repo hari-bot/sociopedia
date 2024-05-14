bd8067c5a2cf61281a71b1abea753d47f06847de
01XASSASSIN <hariharanuv7@gmail.com>
2024-05-14 22:09:29 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -27,12 +27,15 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: "*",
+    origin:
+      "https://sociopedia-frontend-git-main-haris-projects-ccdab74c.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
