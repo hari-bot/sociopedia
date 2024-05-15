130ca0dc91c8d48bae86f057b87eea857d24f3b4
01XASSASSIN <hariharanuv7@gmail.com>
2024-05-15 11:15:35 +0530

    corrected getFriends src link

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `https://sociopedia-api-ten.vercel.app/${userId}/friends`,
+      `https://sociopedia-api-ten.vercel.app/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
